ea0a47b3e1135d5dae127390f3af4836b712ae2d
Karen Ye <35478252+khye99@users.noreply.github.com>
2019-04-09 19:43:17 -0500

    Add files via upload

---
+++ microsite/revised-mockup/app.js
@@ -0,0 +1,10 @@
+function openMenu() {
+    document.getElementById('menu').style.width = '600px';
+    document.getElementById('navBar_content').style.marginLeft = '600px';
+}
+
+function closeMenu() {
+    document.getElementById('menu').style.width = '0';
+    document.getElementById('navBar_content').style.marginLeft = '0';
+}
+
